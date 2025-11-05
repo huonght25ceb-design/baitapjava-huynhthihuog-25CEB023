9562937c8930fbaec01831304280a78107fef906
Huynh Thi Huong <huonght.25ceb@vku.udn.vn>
2025-11-05 21:12:04 +0700

    baitap4.1

---
+++ Cylinder.java
@@ -0,0 +1,28 @@
+package baitap4_1;
+
+public class Cylinder extends Circle {
+    private double height = 1.0;
+
+    public Cylinder() {
+        super();
+        height = 1.0;
+    }
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public double getVolume() {
+        return getArea()*height;
+    }
+    public String toString() {      // in Cylinder class
+        return "Cylinder: subclass of " + super.toString()  // use Circle's toString()
+                + " height=" + height;
+    }
+
+}
